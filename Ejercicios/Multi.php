91ee6bbd5e9ddec4014d3a033f12852f3d3e94d6
Sara-20037 <corazonsitobonito2@gmail.com>
2022-10-27 10:18:40 -0500

    first comit

--- Ejercicios/Multi.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $Num1 = $_POST['Número1'];
-  $Num2 = $_POST['Número2'];
-
-  $Suma = $Num1 * $Num2;
-
-  echo $Suma;
-
-?>
--- Ejercicios/Suma.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $Num1 = $_GET['Num1'];
-    $Num2 = $_GET['Num2'];
-    $Suma = $Num1 + $Num2;
-
-    echo $Suma;
-
-
-?>
